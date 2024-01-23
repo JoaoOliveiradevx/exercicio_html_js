82aec1c38ca1484045b74d957745752a1542b485
Joao Oliveira <joaopgo14@hotmail.com>
2024-01-22 23:32:13 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById('form-deposito')
+
+
+form.addEventListener('submit', function(e){
+  
+    e.preventDefault()
+
+    const campoA = document.getElementById('campoA')
+    const campoB = document.getElementById('campoB')
+    const teste = document.getElementById('teste')
+
+
+    
+    
+    if (parseFloat(campoB.value) > parseFloat(campoA.value)) {
+        alert('Formulário enviado!')
+
+        campoA.value = ''
+        campoB.value = ''
+
+    } else {
+        alert('Ta incompleto!')
+    }
+
+})
